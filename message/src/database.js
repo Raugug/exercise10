afa2285af34d8fb9a04580b43a51e823683b7c88
RaulG <guiaraul@gmail.com>
2018-11-14 11:26:14 +0100

    Copy tested w postman

--- message/src/database.js
+++ message/src/database.js
@@ -2,10 +2,8 @@ const mongoose = require("mongoose");
 mongoose.set('useFindAndModify', false);
 
 const servers = {
-  primary: "exercise8_mongodb_message_1:27027",
-  replica: "exercise8_replica_message_1:27028"
-  //primary: "mongodb_message:27027",
-  //replica: "replica_message:27028"
+  primary: "exercise10_mongodb_message_1:27027",
+  replica: "exercise10_replica_message_1:27028"
   //primary: "127.0.0.1:27027",
   //replica: "127.0.0.1:27028"
 };
